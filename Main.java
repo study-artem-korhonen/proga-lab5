2d6ac8dece4051b523627abe83ffafe99a4fce2c
study-artem-korhonen <study.artem.korhonen@gmail.com>
2025-03-21 17:47:30 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import main.*;
 
 public class Main {
     public static void main(String[] args) {
-        CsvConverter csvConverter = new CsvConverter("cities.csv");
+        CsvConverter csvConverter = new CsvConverter(System.getenv("LAB5_PATH"));
         CollectionManager collectionManager = new CollectionManager(csvConverter);
         Console console = new Console();
         CommandManager commandManager = new CommandManager(collectionManager, console);
